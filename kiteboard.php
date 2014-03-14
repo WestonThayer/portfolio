30e073c0981ae1c4566bb808197eb43f80250aac
westont <weston.thayer@outlook.com>
2014-03-14 12:46:44 -0700

    Kiteboard under control

--- kiteboard.php
+++ kiteboard.php
@@ -1,4 +1,51 @@
 <?php require_once('header.php'); ?>
+<div class="section-custom-light">
+    <div class="container gallery-project">
+        <div class="row">
+            <div class="col-md-8">
+                <img src="/images/kiteboard/action_1.jpg" />
+            </div>
+            <div class="col-md-4 section-hook">
+                <h1>
+                    <small>custom</small>
+                    Kiteboard
+                </h1>
+                <p>
+                    A kiteboard's function is to act as a
+                    <a href="http://en.wikipedia.org/wiki/Daggerboard" target="_blank">daggerboard</a>
+                    for a kiteboarder, resisting the kite's downwind pull and converting a portion
+                    of that force to allow the boarder upwind progress. As a result, they should
+                    be fairly flat. Square edges can also help. Fins are optional, as well as
+                    foot straps and pads.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8">
+                <img src="/images/kiteboard/plans.png" />
+            </div>
+            <div class="col-md-4">
+                <p>
+                    <em>
+                    	To-scale sketches can be measured and translated onto the final product.
+                    </em>
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8">
+                <img src="/images/kiteboard/rocker.jpg" />
+            </div>
+            <div class="col-md-4">
+                <p>
+                    <em>
+                    	A Rocker Table is used to mold shape of the board.
+                    </em>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
 <div id="content">
 	<div id="block">
 		<div id="left-box">
