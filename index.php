1b43cf1f5c1e12f8c350317da03ed6130f68518e
westont <weston.thayer@outlook.com>
2014-03-13 22:34:18 -0700

    RingPack page

--- index.php
+++ index.php
@@ -79,8 +79,8 @@
 		</div>
 	</section>
 </div>
-<div class="section-custom-light">
-	<section id="skills" class="container">
+<div id="skills" class="section-custom-light">
+	<section class="container">
 		<div class="row">
 			<div class="col-md-12 section-hook">
 				<h1>
@@ -90,40 +90,137 @@
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-md-5 section-skills-design">
+			<div class="col-md-4 section-skills-design">
 				<h3>Design</h3>
 				<p>
-					I love the process of exploring the tree of solutions to an
+					I love exploring the possibilities of an
 					open ended, ambigious question. From <strong>user
 					research</strong> - to <strong>prototyping</strong> - to <strong>usability
 					testing</strong>, I'm constantly iterating towards an efficient, ideal
-					solution within the constraints. I'm <a href="http://www.hcde.washington.edu/ucd" target="_blank">
-					educated</a> in user-centered design and this is where my creative side shines.
+					solution within the constraints. I'm
+					<a href="http://www.hcde.washington.edu/ucd" target="_blank">educated</a>
+					in User-Centered Design and my decisions are driven by all kinds of user data. This is
+					where my creative side shines.
 				</p>
 			</div>
-			<div class="col-md-2">
-				<h1 id="header-ampersand">&amp;</h1>
+			<div class="col-md-4">
+				<?php //include('/images/brain.svg') ?>
+				<!--<h1 id="header-ampersand">&amp;</h1>-->
 			</div>
-			<div class="col-md-5">
+			<div class="col-md-4">
 				<h3>Develop</h3>
 				<p>
 					When a question finds an answer, I'm there to help build it into a product.
-					I have experience with
-					<strong>architecting</strong>,
+					I have a
+					<a href="https://www.cs.vt.edu/" target="_blank">CS background</a>
+					and experience with
 					<strong>front-end</strong>,
 					<strong>back-end</strong>, and
 					<strong>testing</strong>.
-					This means I can bring a unique perspective to a team; I understand the brushes
-					and paint of our canvas.
+					Software platforms are intimately familiar to me and I draw from this knowledge
+					to inform and inspire the design. I bring a unique perspective; an understanding
+					of the paint, brushes, and canvas.
 				</p>
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-md-4 col-md-push-4">
+			<div class="col-md-12 quote">
 				<blockquote>
-					&ldquo;I like to wear a lot of hats."
+					I like to wear many hats.
 				</blockquote>
 			</div>
+		</div>
+		<div id="skills-list" class="row">
+			<?php
+				class Skill {
+					public $IsHeader;
+					public $Link;
+					public $Title;
+
+					public function __construct($i, $l, $t) {
+						$this->IsHeader = $i;
+						$this->Link = $l;
+						$this->Title = $t;
+					}
+
+					public function EchoListItem() {
+						echo '<li>';
+
+						if ($this->IsHeader) {
+							echo '<h4>' . $this->Title . '</h4>';
+						}
+						else {
+							echo '<a href="' . $this->Link . '" target="_blank">' . $this->Title . '</a>';
+						}
+
+						echo '</li>';
+					}
+				}
+
+				$skills = array
+				(
+					new Skill(true, '', 'Methods'),
+					new Skill(false, 'http://en.wikipedia.org/wiki/Agile_software_development', 'Agile'),
+					new Skill(false, 'http://en.wikipedia.org/wiki/User-centered_design', 'UCD'),
+					new Skill(false, 'http://en.wikipedia.org/wiki/Don\'t_repeat_yourself', 'DRY'),
+					new Skill(false, 'http://en.wikipedia.org/wiki/Responsive_web_design', 'Responsive Design'),
+					new Skill(false, 'http://en.wikipedia.org/wiki/Model_View_ViewModel', 'MVVM'),
+					new Skill(false, 'http://en.wikipedia.org/wiki/Model%E2%80%93view%E2%80%93controller', 'MVC'),
+					new Skill(false, 'http://en.wikipedia.org/wiki/Representational_state_transfer', 'REST'),
+					new Skill(false, 'http://en.wikipedia.org/wiki/Storyboard', 'Storyboarding'),
+					new Skill(false, 'http://en.wikipedia.org/wiki/Paper_prototyping', 'Paper Prototyping'),
+					new Skill(true, '', 'Front-End'),
+					new Skill(false, 'http://getbootstrap.com/', 'Bootstrap'),
+					new Skill(false, 'compass-style.org', 'Compass'),
+					new Skill(false, 'http://sass-lang.com/', 'Sass'),
+					new Skill(false, 'http://www.w3schools.com/css/css3_intro.asp', 'CSS3'),
+					new Skill(false, 'http://en.wikipedia.org/wiki/XHTML', 'XHTML'),
+					new Skill(false, 'http://en.wikipedia.org/wiki/Extensible_Application_Markup_Language', 'XAML'),
+					new Skill(false, 'http://developer.android.com/guide/topics/ui/declaring-layout.html', 'Android XML'),
+					new Skill(true, '', 'Back-End'),
+					new Skill(false, 'http://en.wikipedia.org/wiki/PHP', 'PHP'),
+					new Skill(false, 'http://en.wikipedia.org/wiki/C_Sharp_(programming_language)', 'C#'),
+					new Skill(false, 'http://en.wikipedia.org/wiki/C++', 'C++'),
+					new Skill(false, 'http://en.wikipedia.org/wiki/Java_(programming_language)', 'Java'),
+					new Skill(false, 'http://en.wikipedia.org/wiki/Objective-C', 'Objective-C'),
+					new Skill(false, 'http://en.wikipedia.org/wiki/SQL', 'SQL'),
+					new Skill(true, '', 'Platforms'),
+					new Skill(false, 'http://msdn.microsoft.com/en-us/windows/apps/br229512.aspx', 'Windows 8'),
+					new Skill(false, 'http://msdn.microsoft.com/en-us/library/windowsphone/develop/ff402535(v=vs.105).aspx', 'Windows Phone'),
+					new Skill(false, 'http://developer.android.com/index.html', 'Android'),
+					new Skill(false, 'http://en.wikipedia.org/wiki/Web_development', 'Web'),
+					new Skill(false, 'http://www.adobe.com/products/flash.html', 'Flash'),
+					new Skill(false, 'https://developer.apple.com/', 'iOS'),
+					new Skill(true, '', 'Tools'),
+					new Skill(false, 'http://www.adobe.com/products/photoshop.html', 'Photoshop'),
+					new Skill(false, 'http://www.adobe.com/products/illustrator.html', 'Illustrator'),
+					new Skill(false, 'http://www.adobe.com/products/indesign.html', 'InDesign'),
+					new Skill(false, 'http://www.adobe.com/products/aftereffects.html', 'After Effects'),
+					new Skill(false, 'http://www.adobe.com/products/premiere.html', 'Premiere'),
+					new Skill(false, 'https://www.ableton.com/', "Ableton Live")
+				);
+			?>
+			<ul class="list-unstyled">
+				<?php
+					$index = 0;
+
+					for ($column = 0; $column < 4; $column++) {
+						if ($column < 2) {
+							echo '<div class="col-xs-3 col-md-2 text-right">';
+						}
+						else {
+							echo '<div class="col-xs-3 col-md-2 col-md-push-4">';
+						}
+
+						for ($i = 0; $index < count($skills) && $i < 10; $i++) {
+							$skills[$index]->EchoListItem();
+							$index++;
+						}
+
+						echo '</div>';
+					}
+				?>
+			</ul>
 		</div>
 		<!--<div class="row">
 			<div class="col-md-4">
--- ringpack.php
+++ ringpack.php
@@ -1,143 +1,96 @@
 <?php require_once('header.php'); ?>
-<div id="content">
-	<div id="left-bar">
-		<a href="http://westonthayer.com/images/ringpack/sc1.png" rel="lightbox[ringpack]"
-			title="The central screen. Choose any pack to begin using, or 'none' to disable. Long pressing yields more options."
-			>
-			<img src="http://westonthayer.com/images/ringpack/sc1_thumb.png"
-				width="228px" height="138px" />
-		</a>
-		<a href="http://westonthayer.com/images/ringpack/sc2.png" rel="lightbox[ringpack]"
-			title="The preferences screen. The 'Accessibility Hack' needs a UX reworking. It is marked as an issue on GitHub."
-			>
-			<img src="http://westonthayer.com/images/ringpack/sc2_thumb.png"
-				width="228px" height="138px" />
-		</a>
-		<a href="http://westonthayer.com/images/ringpack/widget_thumb.png" rel="lightbox[ringpack]"
-			title="The home screen widget."
-			>
-			<img src="http://westonthayer.com/images/ringpack/widget_thumb.png"
-				width="228px" height="138px" />
-		</a>
-		<a href="http://westonthayer.com/images/ringpack/mindmeister.png" rel="lightbox[ringpack]"
-			title="The basic architecture. I later realized the Service could be replaced by a standard Java class that relies on BoadcastReceivers to call it (since everything is trigger based). The green arrows represent a flow of control signals (usually Intents)."
-			>
-			<img src="http://westonthayer.com/images/ringpack/mindmeister_thumb.png"
-				width="228px" height="138px" />
-		</a>
-		<a href="http://www.youtube.com/embed/A9Gga67I_hA"
-			title="YouTube promo video."
-			>
-			<img src="http://westonthayer.com/images/ringpack/youtube_thumb.png"
-				width="228px" height="138px" />
-		</a>
+<div class="section-custom-light">
+	<div class="container gallery-project">
+		<div class="row">
+			<div class="col-md-8">
+				<img src="/images/ringpack/feature_graphic.png" />
+			</div>
+			<div class="col-md-4 section-hook">
+	            <h1>
+	            	<small>android</small>
+	            	RingPack
+	            </h1>
+	            <h4>
+	            	<em>
+	            		<a href="http://cryclops.com/apps/ringpack/" target="_blank">Web</a> |
+						<a href="https://market.android.com/details?id=com.cryclops.ringpack" target="_blank">Market</a>
+					</em>
+				</h4>
+				<p>
+					An app that makes your phone <em>yours</em>, even when it's still in your pocket.
+				</p>
+	        </div>
+		</div>
+		<div class="row">
+			<div class="col-md-8">
+				<iframe width="750" height="422" src="//www.youtube.com/embed/A9Gga67I_hA?rel=0" frameborder="0" allowfullscreen></iframe>
+			</div>
+			<div class="col-md-4">
+				<h2>Research</h2>
+				<p>
+					<em>How can my phone feel like an extension of me?</em>
+				</p>
+				<p>
+					Make it personal. When I researched the
+					<a href="http://lifehacker.com/5875678/how-to-customize-your-iphone-inside-and-out" target="_blank">customization</a>
+					<a href="https://play.google.com/store/search?q=launcher&amp;c=apps" target="_blank">landscape</a>,
+					I was astounded by the variety of options and the fact that almost every major phone platform has a passionate
+					customization community.
+				</p>
+				<p>
+					Affinity diagramming my notes revealed two major categories:
+				</p>
+				<ul>
+					<li>UI modifications</li>
+					<li>Custom ringtones</li>
+				</ul>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-8">
+				<img src="/images/ringpack/onenotewireframe.png" />
+			</div>
+			<div class="col-md-4">
+				<h2>Design</h2>
+				<p>
+					I was inspired by
+					<a href="http://developer.android.com/reference/android/Manifest.permission.html#RECEIVE_SMS" target="_blank">Android&#39;s
+					SMS permission</a>, which fires an event whenever a text is received.
+					Could this be used to deliver a new custom ringtone experience?
+				</p>
+				<p>
+					Rough ideas started on paper, slowly making their way into wireframes (used in the video).
+					A general spec grew along side the sketches in OneNote to help me prioritize features
+					and find the MVP.
+				</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-8">
+				<img src="/images/ringpack/sc1phone.png" />
+			</div>
+			<div class="col-md-4">
+				<h2>Evaluation</h2>
+				<p>
+					Initially, RingPack was going to ship with handful of preselected tones. After talking with
+					users, it became clear that this one-size-fits-all approach would fall short. The idea of
+					installable packs emerged from the feedback.
+				</p>
+				<p>
+					After testing the app on friends, I found that the <code>Get More</code> feature was
+					<a href="http://www.jnd.org/dn.mss/gestural_interfaces_a_step_backwards_in_usability_6.html" target="_blank">undiscoverable</a>
+					under the <code>MENU</code> key. Moving <code>Get More</code> onto the main screen
+					helped people jump right into the customization workflow.
+				</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-4 col-md-push-4 bottom-finish">
+				<p>17,076 <em>active installs</em></p>
+				<p>157.578 <em>downloads</em></p>
+				<a class="btn btn-primary" href="https://github.com/Cryclops/RingPack" target="_blank">GitHub</a>
+			</div>
+		</div>
 	</div>
-	<div id="right-bar">
-		<h3>RingPack</h3>
-		<p><em>Fall 2010 - Spring 2011</em></p>
-		<p><em><a href="http://cryclops.com/apps/ringpack/" target="_blank">Web</a> |
-		<a href="https://market.android.com/details?id=com.cryclops.ringpack" target="_blank">Market</a></em></p>
-		<br />
-		<p>RingPack started as one of my first ambitious projects getting into
-		Android development. I'd been reading the documentation and noticed that
-        any app can request to be notified when a new SMS comes in. This must've
-        stuck in my brain because several weeks later I came up with the idea of
-        a phone that played a different chord out of a progression every time
-        a new text came in.</p>
-		<br />
-		<h4>Initial Spec</h4>
-		<br />
-		<p>After verifying that the idea was feasible, I started by writing (and
-		sketching) up exactly how I wanted the app to function. This started
-		with a statement of the high level idea, which was followed by exploring
-		it deeper. What is a Pack? How do users extend add Packs? How do they
-		enable and disable it? How much can users customize the experience? All
-		of this was written up in OneNote for later use.</p>
-		<br />
-		<p>After I had a fairly clear set of goals, I broke them down into
-		simpler goals. Editing the contents of a Pack will come later; a home
-		screen widget will come later. I decided to implement the core
-		functionality of the app first, then add in the frills. However, I
-		was careful not to forget the frills when designing my data model to
-		ensure that they wouldn't be impossible to implement later.</p>
-		<br />
-		<h4>Software Architecture</h4>
-		<br />
-		<p>I already had a rough idea of the API components needed, I just
-		needed to figure out what responsibilities each had. I accomplished this
-		with a node based graph that's
-		<a href="http://westonthayer.com/images/ringpack/mindmeister.png" rel="lightbox">inspired by UML</a>.
-		Having a big-picture map like this proved extremely useful when I found
-		myself lost in the pursuit of an annoying bug. The map was accompanied
-		by notes about state and logic in OneNote.</p>
-		<br />
-		<h4>Development</h4>
-		<br />
-		<p>Being (fairly) new to Android development, this was the most time
-		consuming portion of the work. I had SQLite, Java threads, Java file IO,
-		and Android UI customizations to learn. Looking back, I was running into walls
-		by walls that seem obvious now. If I ever have enough time, I owe myself
-		a re-write. The good news is that I now practice much better methods
-		with a new perspective.</p>
-		<br />
-		<h4>Interaction Design</h4>
-		<br />
-		<p>The app opens for the first time and displays an about dialog that provides some
-		background and usage tips. It appears on every launch unless the user tells it not to.
-		The idea was to model the way Apple's iLife products display a welcome
-		screen on start up, encouraging users to learn more about the app.</p>
-		<br />
-		<p>I tried to keep everything as simple as I could, but looking back,
-		this is an area that needs work. People hate reading long walls of text
-		in most scenarios. I believe a tutorial that interactively helps explain
-		RingPack would be ideal and would help with the frustration some users
-		have expressed.</p>
-		<br />
-		<p>From there, a list of installed packs are presented. This seems to
-		work OK, however I think pack-like icons in a grid would serve the Pack
-		metaphor much better and give a clearer window at the data model.</p>
-		<br />
-		<p>Some other issues: the long press action is difficult to discover,
-		discovery of Android's MENU key should not be
-		<a href="http://www.jnd.org/dn.mss/gestural_interfaces_a_step_backwards_in_usability_6.html" target="_blank">assumed</a>,
-		and the process of adding new Packs can be unclear. Overall, I don't
-		feel that I did the UX justice, and it is visible in the user reviews.
-		However, I feel that it was a valuable learning experience.</p>
-		<br />
-		<h4>Deployment and Maintenance</h4>
-		<br />
-		<p>The first release had a major bug. I had a misconception about
-		the life cycle of
-		<a href="http://developer.android.com/reference/android/app/Service.html" target="_blank">Services</a>.
-		I thought that onDestroy() was guaranteed to be called if my Service
-		was to leave memory. I would write all my state information to disk
-		there, which worked fine on my Droid X, but failed miserably on lower
-		performance Android phones that were scrambling for extra memory. Thus
-		was my introduction to
-		<a href="http://www.zdnet.com/blog/open-source/androids-biggest-worry-fragmentation/8022" target="_blank">Android
-		fragmentation</a>.</p>
-		<br />
-		<p>After becoming aware of this via stack traces and support emails, I
-		ended up pulling the first version off of the Market and doing a major
-		overhaul, determined to find out what was wrong. It was released again
-		to a slightly better reception, although some users produced bugs that
-		I could not reproduce on my device.</p>
-		<br />
-		<p>At this point the semester had ended, and I no longer had time to
-		work on the project. I
-		<a href="https://github.com/Cryclops/RingPack" target="_blank">open
-		sourced</a> it under the GPL and invited the community to fix the phone
-		specific bugs and take over the project.</p>
-		<br />
-		<h4>Future</h4>
-		<br />
-		<p>The future is uncertain. I became involved in other work and the
-		community hasn't contributed to the project as of yet. I would love to
-		improve the interaction design above all else as soon as I am able.</p>
-		<br />
-		<p>RingPack currently has ~18,000 active installs, and has an average
-		rating of 3.6 stars. ~150,000 people have tried the app.</p>
-	</div>
-	<div class="clearfix"></div>
 </div>
 <?php require_once('footer.php'); ?>
