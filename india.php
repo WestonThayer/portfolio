2d67f8f362818a16915b77a7dda785d6123105b1
westont <weston.thayer@outlook.com>
2014-03-14 12:09:13 -0700

    India page under control

--- india.php
+++ india.php
@@ -1,119 +1,52 @@
 <?php require_once('header.php'); ?>
-<div id="content">
-	<div id="left-bar">
-		<a href="http://westonthayer.com/images/india/screen.png" rel="lightbox[india]"
-			title="The ideal ledger."
-			>
-			<img src="http://westonthayer.com/images/india/screen.png"
-				width="228px" height="138px" />
-		</a>
-		<a href="http://westonthayer.com/images/india/1.png" rel="lightbox[india]"
-			title="The home screen with no groups added."
-			>
-			<img src="http://westonthayer.com/images/india/1.png"
-				width="228px" height="138px" />
-		</a>
-		<a href="http://westonthayer.com/images/india/2.png" rel="lightbox[india]"
-			title="How to add a group."
-			>
-			<img src="http://westonthayer.com/images/india/2.png"
-				width="228px" height="138px" />
-		</a>
-		<a href="http://westonthayer.com/images/india/3.png" rel="lightbox[india]">
-			<img src="http://westonthayer.com/images/india/3.png"
-				width="228px" height="138px" />
-		</a>
-		<a href="http://westonthayer.com/images/india/4.png" rel="lightbox[india]"
-			title="How to add a member."
-			>
-			<img src="http://westonthayer.com/images/india/4.png"
-				width="228px" height="138px" />
-		</a>
-		<a href="http://westonthayer.com/images/india/5.png" rel="lightbox[india]"
-			title="Sample members. The facilitator is encouraged to take pictures of each member when adding."
-			>
-			<img src="http://westonthayer.com/images/india/5.png"
-				width="228px" height="138px" />
-		</a>
-		<a href="http://westonthayer.com/images/india/6.png" rel="lightbox[india]"
-			title="The ledger after the first meeting with no loans out."
-			>
-			<img src="http://westonthayer.com/images/india/6.png"
-				width="228px" height="138px" />
-		</a>
-		<a href="http://westonthayer.com/images/india/7.png" rel="lightbox[india]"
-			title="Taking out a loan."
-			>
-			<img src="http://westonthayer.com/images/india/7.png"
-				width="228px" height="138px" />
-		</a>
-	</div>
-	<div id="right-bar">
-		<h3>laXmi</h3>
-		<p><em>Fall 2011</em></p>
-		<p><em><a href="https://github.com/Cryclops/IndiaTablet" target="_blank">GitHub</a></em></p>
-		<br />
-		<p>IndiaTablet (name to be improved) is an Android application designed for a class at
-		Virginia Tech called <em>Design for the Bottom of the Pyramid</em>.
-		The idea is to combine multiple disciplines with the common goal of
-		designing and developing something that will help the developing
-		world.</p>
-		<br />
-		<p>I joined a group that was picking up after a project named
-		<em>laXmi</em> that developed a teaching aid for women interested
-		in learning more about microfinancing techniques in India. They
-		identified and addressed several problems they found with the learning
-		process - chiefly they provided a visual way to simulate the impact of
-		interest rates over time. You can learn more about the project
-		<a href="http://www.id4learning.com/laxmi.html" target="_blank">here,
-		as well as watch a video of me explaining the app</a>. I don't give much
-		background on India's microfinancing techniques here, so please forgive
-		me if some of the follow is unclear.</p>
-		<br />
-		<p>Our group re-evaluated the issues and how the current design
-		addresses them. We decided that the <em>laXmi</em> solution
-		efficiently solved a few issues, but ignored others. We focused on a
-		select few:</p>
-		<ul>
-			<li>Groups tend to keep the habit of holding on to their pooled
-			money once it becomes sizable. This is a bad habit since money not
-			loaned is money not earning interest. Case studies show that the
-			most successful microfinancing groups make a point to loan out as
-			much as possible.</li>
-			<li>Groups are often discouraged after the first year because they
-			barely break even (subsequent years are exponentially better). The
-			<em>laXmi</em> simulation partially helps with that but falls
-			short due to there being too many numbers to keep track of. Writing
-			down transactions by hand becomes unmanagable during
-			simulations.</li>
-		</ul>
-		<br />
-		<h4>Design</h4>
-		<br />
-		<p>After several iterations, we came up with the idea of using India's
-		new
-		<a href="http://aakashtablet.org/" target="_blank">Aakash tablet</a>
-		as a tool to help the facilitator teach in combination with the current
-		<em>laXmi</em> solution. The NGO facilitator uses our application as a tool
-		to keep track of groups and simulations.</p>
-		<br />
-		<p>Before, facilitators had to write transactions down and keep
-		accounting by hand - distracting them from the fast paced simulation in
-		front of them. Now, they can simply follow along with the meetings,
-		keeping track with a few clicks. Calculations are done for them and
-		the software runs an idealistic side-by-side account of how they group
-		could improve.</p>
-		<br />
-		<h4>Future</h4>
-		<br />
-		<p>According to our teacher, the <em>laXmi</em> system has been
-		officially adopted by the
-		<a href="http://wcd.nic.in/" target="_blank">Ministry of Women and Child
-		Development</a>. It is a finalist in the
-		<a href="http://worlddesignimpact.org/projects/project/35/" target="_blank">World
-		Design Impact competition</a>. My work is open source on GitHub and it
-		is our hope that the community will pick it up and improve it!</p>
-	</div>
-	<div class="clearfix"></div>
+<div class="section-custom-light">
+    <div class="container gallery-project">
+        <div class="row">
+            <div class="col-md-8">
+                <img src="/images/india/promo.png" />
+            </div>
+            <div class="col-md-4 section-hook">
+                <h1>
+                    <small>android</small>
+                    laXmi
+                </h1>
+                <h4>
+                    <em>
+                        <a href="http://www.id4learning.com/laxmi.html" target="_blank">Web</a>
+                    </em>
+                </h4>
+                <p>
+                    A piece of the
+                    <a href="http://www.id4learning.com/laxmi.html" target="_blank">laXmi solution</a>,
+                    designed to bring illiterate Indian women out of poverty.
+                </p>
+                <p>
+                	I worked with two Industrial Designers at Virginia Tech to design and buld an app for the $50
+                	<a href="http://en.wikipedia.org/wiki/Aakash_(tablet)" target="_blank">Aakash tablet</a>.
+                	The laXmi system has been officially adopted by the
+                	<a href="http://wcd.nic.in/" target="_blank">Ministry of Women and Child Development</a>.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8">
+                <img src="/images/india/screenshot.png" />
+            </div>
+            <div class="col-md-4">
+                <p>
+                    <em>
+                    	The app follows along with group meetings, playing the accountant.
+                    </em>
+                </p>
+            </div>
+        </div>
+        <div class="row">
+			<div class="col-md-4 col-md-push-4 bottom-finish">
+				<a class="btn btn-default" href="https://github.com/Cryclops/RingPack" target="_blank">
+					<img class="btn-icon" src="/images/GitHub-Mark-32px.png"/> GitHub
+				</a>
+			</div>
+		</div>
+    </div>
 </div>
 <?php require_once('footer.php'); ?>
