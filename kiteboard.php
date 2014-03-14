e9d553dcf7fcfbb87c0526b9b2ea9b992a06ebc6
westont <weston.thayer@outlook.com>
2014-03-14 14:58:08 -0700

    Kiteboard fix

--- kiteboard.php
+++ kiteboard.php
@@ -18,6 +18,10 @@
                     be fairly flat. Square edges can also help. Fins are optional, as well as
                     foot straps and pads.
                 </p>
+                <p>
+                	The primary motivator for this project was cost reduction. Commercial kiteboards cost
+                	anywhere between $300 - $600. My materials totaled $133.56.
+                </p>
             </div>
         </div>
         <div class="row">
@@ -45,102 +49,5 @@
             </div>
         </div>
     </div>
-</div>
-<div id="content">
-	<div id="block">
-		<div id="left-box">
-			<h3>Custom Kiteboard</h3>
-			<p><em>2006</em></p>
-			<br />
-			<p>The last three projects I have on here have nothing to do with
-			software. I put them on here because they have a lot to do with
-			how I design and learn. On this page, I'll tell you a bit about a kiteboard
-			that I built back in high school. It involves some fairly detailed
-			plans and processes.</p>
-			<br />
-			<p>A kiteboard is a fairly simple thing. Its function is to act as
-			a dagger-board for a kiteboarder, resisting the kite's downwind pull
-			and converting a portion of that force to allow the boarder upwind
-			progress. As a result, they should be fairly flat. Fins are
-			optional, as well as foot straps and pads. Basically anything will
-			work as long as it's durable and floaty!</p>
-		</div>
-		<div id="right-box">
-			<img src="http://westonthayer.com/images/kiteboard/kiteboard.jpg"
-			width="690px" height="460px" />
-			<p>A fun day in the Atlantic. Dewey Beach, DE.</p>
-		</div>
-		<div class="clearfix"></div>
-	</div>
-	<div id="block">
-		<div id="left-box">
-			<h4>Planning</h4>
-			<br />
-			<p>This project started as many of mine usually do. I wanted a new
-			kiteboard, but I was too cheap to buy a new one. So, I started
-			building a price list to see how much making my own would cost (new
-			kiteboards are ~$300-600). This began with searching for as many
-			forum posts and how-to's that I could find. It was hard because
-			there weren't too many. I had to infer building tips from windsurf
-			board building tutorials, which let to some bad assumptions
-			later.</p>
-			<br />
-			<p>In any case, I build a list of needed materials and estimated
-			cost. The total came to $133.56 + shipping per board. Not bad at
-			all. Of course, that didn't take my time into account, but I was in
-			high school. My time was cheap.</p>
-		</div>
-		<div id="right-box">
-			<img src="http://westonthayer.com/images/kiteboard/plans.png"
-			width="690px" height="446px" />
-			<p>These served as the rough plans, but I followed more detailed
-			plans that
-			<a href="http://westonthayer.com/images/kiteboard/noonoo.pdf"
-			target="_blank">Noo Noo said would work</a>.</p>
-		</div>
-		<div class="clearfix"></div>
-	</div>
-	<div id="block">
-		<div id="left-box">
-			<h4>Rocker and Vacuum Bagging</h4>
-			<br />
-			<p>There's a lot of debate on what rocker, or curve, is best for a
-			board. I read as much as I could and went with an average of what
-			I'd read. I also decided to laminate via vacuum bagging, a process
-			that bonds materials together in a bag under pressure, so I had to
-			build a rocker table. The table is an inverse mold of the board's
-			bottom -- made out of supports and 8mm Luan from Home Depot.</p>
-		</div>
-		<div id="right-box">
-			<img src="http://westonthayer.com/images/kiteboard/rocker.jpg"
-			width="690px" height="250px" />
-			<p>The rocker table consists of supports at specific heights and
-			plywood stretched over them.</p>
-		</div>
-		<div class="clearfix"></div>
-	</div>
-	<div id="block">
-		<div id="left-box">
-			<h4>Conclusion</h4>
-			<br />
-			<p>I could talk more about the process involved, and if you're
-			interested in that, please check out the guide I posted
-			<a href="http://www.eastkb.com/forum/viewtopic.php?t=1436"
-			target="_blank">here (excuse the broken images)</a>. I made some
-			mistakes that caused the board to be a bit too heavy, but it worked
-			well enough. I think this project shows my ability to learn and
-			practice what I read. Vacuum bagging and laminating are tough
-			skills, but they end up saving time in the long run (and luckily
-			come with plenty of documentation).</p>
-		</div>
-		<div id="right-box">
-			<img src="http://westonthayer.com/images/kiteboard/final.jpg"
-			width="690px" height="250px" />
-			<p>The finished board at home an in Exuma, Bahamas. The
-			discoloration spots are due to me being to cheap to purchace clear
-			Epoxy.</p>
-		</div>
-		<div class="clearfix"></div>
-	</div>
 </div>
 <?php require_once('footer.php'); ?>
