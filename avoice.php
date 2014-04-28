aca8fe0b076f28cd7e687855771f958ea09d4a5d
westont <weston.thayer@outlook.com>
2014-04-28 11:39:18 -0700

    Switch to Gfycat for aVoice
    Smoother loading

--- avoice.php
+++ avoice.php
@@ -3,10 +3,7 @@
     <div class="container gallery-project">
         <div class="row">
             <div class="col-md-8">
-                <video class="img-responsive" autoplay="autoplay" loop>
-                    <source src="/images/avoice/1.mp4" type="video/mp4"/>
-                    <img src="/images/avoice/1.png" />
-                </video>
+                <img class="gfyitem" data-title=true data-autoplay=true data-controls=false data-expand=false data-id="AmusingNaturalKangaroo" />
             </div>
             <div class="col-md-4 section-hook">
                 <h1>
