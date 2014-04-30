1fffb310e32e88a1a2f3cdbefe1bbbfe4ec8af63
westont <weston.thayer@outlook.com>
2014-04-29 22:23:18 -0700

    Update AR
    Get's an awesome gif and a storyboard

--- ar.php
+++ ar.php
@@ -3,7 +3,7 @@
     <div class="container gallery-project">
         <div class="row">
             <div class="col-md-8">
-                <img src="/images/ar/ar_winner.jpg" />
+                <img class="gfyitem" data-title="true" data-autoplay="true" data-controls="false" data-expand="true" data-id="DefinitiveSeriousGrayreefshark" />
             </div>
             <div class="col-md-4 section-hook">
                 <h1>
@@ -13,26 +13,29 @@
                 <p>
                     A proof-of-concept
                     <a href="http://fastfooddude.files.wordpress.com/2012/04/cracker1.jpg" target="_blank">Cracker
-                    Jack prize</a> designed to promote Qualcomm's mobile
+                    Jack prize</a> designed to promote Qualcomm's
                     <a href="http://www.qualcomm.com/snapdragon" target="_blank">Snapdragon</a>
-                    processor. This app would be featured on a cereal box.
+                    mobile processor. This app would be featured on a cereal box.
                 </p>
                 <p>
-                    Start the app, point the camera at a special graphic on the cereal box, and watch as a digital world emerges
-                    to tell you if you're a winner.
-                </p>
-                <p>
-                    <i>
-                        Built with the
-                        <a href="https://developer.qualcomm.com/develop/mobile-technologies/augmented-reality" target="_blank">QCAR SDK</a> in
-                        <a href="http://unity3d.com/" target="_blank">Unity3D</a> for a Qualcomm intern project.
-                    </i>
+                    I built the app with the
+                    <a href="https://developer.qualcomm.com/develop/mobile-technologies/augmented-reality" target="_blank">QCAR SDK</a> in
+                    <a href="http://unity3d.com/" target="_blank">Unity3D</a> for a Qualcomm intern project.
                 </p>
             </div>
         </div>
         <div class="row">
             <div class="col-md-8">
-                <img src="/images/ar/ar_loser.png" />
+                <img src="/images/ar/storyboard.jpg" />
+            </div>
+            <div class="col-md-4">
+                <p>
+                    <i>
+                        <a href="http://en.wikipedia.org/wiki/Storyboard" target="_blank">Storyboarding</a>
+                        helped quickly explain the concept to others. By the end of the experience,
+                        Nick wants a Snapdragon powered phone.
+                    </i>
+                </p>
             </div>
         </div>
     </div>
